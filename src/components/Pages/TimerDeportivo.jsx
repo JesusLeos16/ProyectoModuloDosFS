6e203ef8bc76e0975d28406b670cb48d79e3bcba
JesusLeos16 <jelm060716@gmail.com>
2026-03-04 19:53:03 -0600

    Agrega estados temporales a TimerDeportivo.jsx

--- src/components/Pages/TimerDeportivo.jsx
+++ src/components/Pages/TimerDeportivo.jsx
@@ -1,10 +1,19 @@
 //todo el merequetengue
 import { useState, useEffect } from "react";
+//Funcion temporal de como deberia funcionar este timer, es para hacer pruebas tambien, mas adelante ya vamos a mandar
+//parametros, amonos por partes.
 export default function TimerDeportivo() {
 
-  return (
-    <>
-    
-    </>
-  );
+  const [rondasTotales, setRondasTotales] = useState(3);
+  const [tiempoRonda, setTiempoRonda] = useState(60);
+
+  const [tiempo, setTiempo] = useState(0);
+  const [corriendo, setCorriendo] = useState(false);
+  const [rondaActual, setRondaActual] = useState(1);
+
+  const [fase, setFase] = useState("configurando");
+
+
+
+  return <></>;
 }
