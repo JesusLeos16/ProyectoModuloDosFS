61eb1ff67464917855618a0fab0058dc7d67e992
JesusLeos16 <jelm060716@gmail.com>
2026-03-05 21:04:17 -0600

    Agrega interfaz de configuracion sin funcionalidad

--- src/components/Pages/TimerDeportivo.jsx
+++ src/components/Pages/TimerDeportivo.jsx
@@ -24,5 +24,24 @@ export default function TimerDeportivo() {
     setFase("configuracion");
   };
 
+  if (fase == "configurando") {
+    return (
+      <div className="flex flex-col h-screen justify-center items-center bg-white-700 text-2xl font-sans">
+        <div className="bg-white text-black p-6  flex flex-col gap-10 rounded-lg ">
+          <h1 className="font-semibold">CONFIGURAR LA RUTINA</h1>
+          <div className=" p-4 rounded-lg shadow-sm shadow-amber-300 flex justify-between">
+            <p>Rondas </p>
+            <p>{rondasTotales}</p>
+          </div>
+         <div className=" p-4 rounded-lg shadow-sm shadow-amber-300 flex justify-between">
+            <p>Tiempo/ronda </p>
+            <p>{tiempoRonda}s</p>
+          </div>
+          <button onClick={iniciarEntrenamiento} className="bg-amber-300 text-white font-bold p-3 rounded-2xl transition-all duration-300 hover:-translate-y-2 hover:shadow-lg">EMPEZAR ENTRENAMIENTO</button>
+        </div>
+      </div>
+    );
+  }
+
   return <></>;
 }
