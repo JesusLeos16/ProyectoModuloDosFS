5b6fd79d5e3875809b184b30895e75908305ed9e
JesusLeos16 <jelm060716@gmail.com>
2026-03-05 22:10:16 -0600

    Agrega controles con funcionalidad en TimerDeportivo.jsx

--- src/components/Pages/TimerDeportivo.jsx
+++ src/components/Pages/TimerDeportivo.jsx
@@ -1,7 +1,6 @@
 //todo el merequetengue
 import { useState, useEffect } from "react";
-//Funcion temporal de como deberia funcionar este timer, es para hacer pruebas tambien, mas adelante ya vamos a mandar
-//parametros, amonos por partes.
+import PanelControl from "../molecules/PanelControl";
 export default function TimerDeportivo() {
   const [rondasTotales, setRondasTotales] = useState(3);
   const [tiempoRonda, setTiempoRonda] = useState(60);
@@ -12,25 +11,56 @@ export default function TimerDeportivo() {
 
   const [fase, setFase] = useState("configurando");
 
-  const subrRondas = () => setRondasTotales(rondasTotales + 1);
+  const subirRondas = () => setRondasTotales(rondasTotales + 1);
   const bajarRondas = () => {
     if (rondasTotales > 1) setRondasTotales(rondasTotales - 1);
   };
   const subirTiempo = () => setTiempoRonda(tiempoRonda + 5);
-  const bajarTiempo = () => {if(tiempoRonda>5) setTiempoRonda(tiempoRonda-5)};
+  const bajarTiempo = () => {
+    if (tiempoRonda > 5) setTiempoRonda(tiempoRonda - 5);
+  };
 
   const cancelarEntrenamiento = () => {
     setCorriendo(false);
-    setFase("configuracion");
+    setFase("configurando");
   };
 
   if (fase == "configurando") {
     return (
-     <div>
-      
-     </div>
+      <div>
+        <h1>CONFIGURAR RUTINA</h1>
+        {/* rondas */}
+        <PanelControl
+          titulo="Rondas"
+          valor={rondasTotales}
+          Suma={subirRondas}
+          Resta={bajarRondas}
+          Tiempo={false}
+        />
+        {/* tiempo */}
+        <PanelControl
+        titulo="Tiempo"
+        valor={tiempoRonda}
+        Suma={subirTiempo}
+        Resta={bajarTiempo}
+        Tiempo={true}
+        />
+
+        <button>COMENZAR</button>
+
+      </div>
     );
   }
 
-  return <></>;
+  return(
+    <>
+    <div className={`${fase=='preparacion'? 'bg-amber-300':''} ${fase=='entrenando'?'bg-emerald-400 text-black':'' }`}>
+
+      <h1>{tiempo}</h1>
+      <h2>{fase}</h2>
+      <button>Cancelar</button>
+    </div>
+
+    </>
+  );
 }
